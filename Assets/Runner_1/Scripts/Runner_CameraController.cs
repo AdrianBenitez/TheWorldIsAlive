bf230c83b538a03832119262eb22ffa2f860b44f
benmarad <benmarad81@outlook.com>
2018-06-25 18:13:09 -0700

    Runner_1

---
+++ Assets/Runner_1/Scripts/Runner_CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Runner_CameraController : MonoBehaviour 
+{
+    public GameObject mainCharacter;
+    private Vector3 offset; 
+	
+    void Start()
+    {
+        offset = new Vector3(transform.position.x, transform.position.y, mainCharacter.transform.position.z -12f);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = mainCharacter.transform.position + offset;
+    }
+}
---
+++ Assets/Runner_1/Scripts/Runner_PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner_PlayerController : MonoBehaviour 
+{
+    public float speed;
+
+    void Start () 
+    {
+    }
+    
+    void Update () 
+    {
+        updateVelocity();
+        updateControllInput();
+    }
+
+    private void updateVelocity()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void updateControllInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position = 
+                Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                            new Vector3(transform.position.x + 1, transform.position.y, transform.position.z),
+                            Time.time * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        { 
+            transform.position = 
+                Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                            new Vector3(transform.position.x - 1, transform.position.y, transform.position.z),
+                            Time.time * speed);
+        }
+    }
+}
