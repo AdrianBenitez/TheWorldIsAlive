38d50bbe2cfb9f41643f190f76189e6bebe67a75
gatiz1m0 <orlandroide@gmail.com>
2018-06-27 14:54:15 -0700

    I added some coments for the house scene.
    Just to make it a little easier if someone wants to mess with it.

--- Assets/Scripts/mageController.cs
+++ Assets/Scripts/mageController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Re-using some code from Adrian here.
+ * Also I'm using the character and animations from here: https://youtu.be/_rrcoptdnqE
+ * I did it just temporarily because I don't now (yet) how to rig characters for mecanim
+ */
+
 public class mageController : MonoBehaviour {
+    // Change speed at your desire.
     public float speed = -2f;
     Animator anim;
-    bool holdingDown;
+    bool holdingDown; // To know when a key is being pressed, if no key is being pressed hero should go to idle animation.
 
     // Use this for initialization
     void Start () {
@@ -15,11 +21,12 @@ public class mageController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(0f, 0f, Input.GetAxis("Horizontal") * Time.deltaTime * speed);
-        Input_Control();
+        Input_Control(); // Do something when keys are pressed.
 	}
 
     void Input_Control()
     {
+        // Move hero to right
         if ( Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)  )
         {
             holdingDown = true;
@@ -30,6 +37,7 @@ public class mageController : MonoBehaviour {
             anim.SetBool("idle", false);
             anim.SetBool ("move", true);   
         }
+        // Move hero to left
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             holdingDown = true;
@@ -39,6 +47,7 @@ public class mageController : MonoBehaviour {
             anim.SetBool("idle", false);
             anim.SetBool("move", true);
         }
+        // If no keys are being pressed go to idle.
         if (!Input.anyKey && holdingDown)
         {
             //Debug.Log("A key was released");
--- Assets/StairFloorTrigger.cs
+++ Assets/StairFloorTrigger.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Very important to put enemies on the z axis at 2.5f so the enemies can interact with hero.
+ * otherwise if character is on the z = 1.5f enemies cannot harm it. So the stairs is a safe zone by default.
+*/ 
 public class StairFloorTrigger : MonoBehaviour {
     GameObject character;
 	// Use this for initialization
@@ -14,11 +17,11 @@ public class StairFloorTrigger : MonoBehaviour {
         Debug.Log("Something triggered stairfloortrigger");
         if (other.tag == "Player")
         {
-            if (character.transform.position.z == 1.5f)
+            if (character.transform.position.z == 1.5f) // if hero is on the stairs z axis then bring hero to front, away from stairs
             {
                 character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, 2.5f);
             }
-            else
+            else // if the hero is not aligned with z axis of stairs bring hero back so hero can use stairs
             {
                 character.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, 1.5f);
             }
