1d4c9e144586e93cd4e61a933fcac2d4a84404c3
gatiz1m0 <orlandroide@gmail.com>
2018-06-27 22:18:38 -0700

    Just added a trigger collider at end of house minigame
    The collider loads the next scene "Runner" but you can change that to a text scene or something.

---
+++ Assets/EndOfLevelTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndOfLevelTrigger : MonoBehaviour {
+
+    GameObject character;
+    // Use this for initialization
+    void Start()
+    {
+        character = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("Runner");
+    }
+}
