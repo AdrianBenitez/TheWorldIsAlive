3202aceda9976e5b9612798c699e8f6cab8e2d40
benmarad <benmarad81@outlook.com>
2018-06-30 10:07:26 -0700

    new character for runner & ligthning probems

--- Assets/PlayerOnRunnerSceneControl.cs
+++ Assets/PlayerOnRunnerSceneControl.cs
@@ -39,9 +39,23 @@ public class PlayerOnRunnerSceneControl : MonoBehaviour {
 
     void Input_Control()
     {
-        if (anim.GetBool("move") == false)
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            anim.SetBool("move", true);
+            var newX = transform.position.x + 1;
+            if (newX > 1f) { newX = 1f; }
+            transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                                            new Vector3(newX, transform.position.y, transform.position.z),
+                                              Time.time * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            var newX = transform.position.x - 1;
+            if (newX < -1f) { newX = -1f; }
+
+            transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+                                            new Vector3(newX, transform.position.y, transform.position.z),
+                                              Time.time * speed);
         }
     }
 
@@ -54,6 +68,18 @@ public class PlayerOnRunnerSceneControl : MonoBehaviour {
             anim.SetBool("die", true);
             isDead = true;
             StartCoroutine(Respawn(respawnDelay));
+        }
+    }
+
+    void OnAnimatorMove()
+    {
+        Animator animator = GetComponent<Animator>();
+
+        if (animator)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.z += speed * Time.deltaTime;
+            transform.position = newPosition;
         }
     }
 
